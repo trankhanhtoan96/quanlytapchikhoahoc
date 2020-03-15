d6980cab43730bac33d661d2cfbc6dae6cc9539b
tkt96 <trankhanhtoan96@gmail.com>
2020-03-15 17:08:04 +0700

    first commit!

--- asokalaw/routers/home.php
+++ asokalaw/routers/home.php
@@ -3,5 +3,5 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 $app->get('/', function (ServerRequestInterface $rq, ResponseInterface $rs, array $ag) {
-    return 'home';
+    return $this->view->render($rs, 'asokalaw/home/home.twig', $ag);
 });
