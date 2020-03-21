f6aa901506e00c59d2be336c58c5e446a7f170f3
tkt96 <trankhanhtoan96@gmail.com>
2020-03-21 11:38:24 +0700

    tkt

--- asokalaw/routers/home.php
+++ asokalaw/routers/home.php
@@ -1,7 +1,15 @@
 <?php
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 
-$app->get('/', function (ServerRequestInterface $rq, ResponseInterface $rs, array $ag) {
+use Psr\Http\Message\ResponseInterface as RI;
+use Psr\Http\Message\ServerRequestInterface as SR;
+
+$app->get('/', function (SR $rq, RI $rs, array $ag) {
+    $ag['partners'] = array();
+    $tmp = scandir('upload/files/partner');
+    foreach ($tmp as $tmp2) {
+        if (is_file('upload/files/partner/' . $tmp2)) {
+            $ag['partners'][] = $GLOBALS['config']['base_url'] . '/upload/files/partner/' . $tmp2;
+        }
+    }
     return $this->view->render($rs, 'asokalaw/home/home.twig', $ag);
 });
