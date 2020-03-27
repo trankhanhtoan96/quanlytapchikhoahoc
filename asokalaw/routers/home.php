63c943c5ffdce985fa73ab4e8bb361f17109db0e
tkt96 <trankhanhtoan96@gmail.com>
2020-03-28 03:33:21 +0700

    tkt

--- asokalaw/routers/home.php
+++ asokalaw/routers/home.php
@@ -11,5 +11,15 @@ $app->get('/', function (SR $rq, RI $rs, array $ag) {
             $ag['partners'][] = $GLOBALS['config']['base_url'] . '/upload/files/partner/' . $tmp2;
         }
     }
-    return $this->view->render($rs, 'asokalaw/home/home.twig', $ag);
+    return $this->view->render($rs, 'asokalaw/home/index.twig', $ag);
+});
+$app->get('/dang-ky-nhan-hieu', function (SR $rq, RI $rs, array $ag) {
+    $ag['partners'] = array();
+    $tmp = scandir('upload/files/partner');
+    foreach ($tmp as $tmp2) {
+        if (is_file('upload/files/partner/' . $tmp2)) {
+            $ag['partners'][] = $GLOBALS['config']['base_url'] . '/upload/files/partner/' . $tmp2;
+        }
+    }
+    return $this->view->render($rs, 'asokalaw/dknh/index.twig', $ag);
 });
